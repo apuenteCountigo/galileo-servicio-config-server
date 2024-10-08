76eb71fd800aa561bf5e65a1381041f4bdb70fa9
apuentes+1-at-178934116267 <apuente@soaint.com>
2024-10-08 17:53:46 -0400

    8-INIT

--- servicio-config-server/src/main/java/com/formacionbdi/springboot/app/config/SpringbootServicioConfigServerApplication.java
+++ servicio-config-server/src/main/java/com/formacionbdi/springboot/app/config/SpringbootServicioConfigServerApplication.java
@@ -16,7 +16,7 @@ public class SpringbootServicioConfigServerApplication implements CommandLineRun
 	@Override
 	public void run(String... args) throws Exception {
 		System.out.println("**************************************");
-		System.out.println("CONFIG V-241008 17:49");
+		System.out.println("CONFIG V-241008 17:53");
 	}
 
 }
