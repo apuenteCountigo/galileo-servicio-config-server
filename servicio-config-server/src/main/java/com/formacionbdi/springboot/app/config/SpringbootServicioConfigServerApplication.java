0a64c24ef32b799aec1b3760350738e9eff572c1
apuentes+1-at-178934116267 <apuente@soaint.com>
2024-10-08 16:23:32 -0400

    5-INIT

--- servicio-config-server/src/main/java/com/formacionbdi/springboot/app/config/SpringbootServicioConfigServerApplication.java
+++ servicio-config-server/src/main/java/com/formacionbdi/springboot/app/config/SpringbootServicioConfigServerApplication.java
@@ -1,15 +1,22 @@
 package com.formacionbdi.springboot.app.config;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
 @EnableConfigServer
 @SpringBootApplication
-public class SpringbootServicioConfigServerApplication {
+public class SpringbootServicioConfigServerApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootServicioConfigServerApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		System.out.println("**************************************");
+		System.out.println("CONFIG V-241008 16:23");
 	}
 
 }
